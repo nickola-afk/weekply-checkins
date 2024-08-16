d39b5687232181fada8511d276e32297c5d614b5
nickola-afk <may562@evit.edu>
2024-08-16 12:23:50 -0700

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,97 @@
+Python 3.12.4 (tags/v3.12.4:8e8a4ba, Jun  6 2024, 19:30:16) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/may562/Documents/GitHub/oof/smth.py
+hi give me a word for someone to guess: WOat
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+hey this is hangman guess a letter: w
+good
+w
+_
+_
+_
+['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z']
+hey this is hangman guess a letter: o
+good
+w
+o
+_
+_
+['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z']
+hey this is hangman guess a letter: a
+good
+w
+o
+a
+_
+['b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z']
+hey this is hangman guess a letter: t
+good
+w
+o
+a
+t
+ you win 
+['b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 'u', 'v', 'x', 'y', 'z']
